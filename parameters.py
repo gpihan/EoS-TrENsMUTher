86a934aaeb8205cd53365303f6db4b04bd63490b
Gregoire Pihan <g.pihan@gmail.com>
2024-08-30 18:16:56 -0400

    Add Paramaters Class

--- parameters.py
+++ parameters.py
@@ -1,18 +1,18 @@
-parameter_dict = {
-    "edens_min" = 0.001, # GeV/fm^3
-    "edens_max" = 0.5, # GeV/fm^3
-    "nB_min" = 0.01, # 1/fm^3
-    "nB_mmax" = 0.01, # 1/fm^3
-    "nQ_min" = 0.01, # 1/fm^3
-    "nQ_mmax" = 0.01, # 1/fm^3
-    "nS_min" = 0.01, # 1/fm^3
-    "nS_mmax" = 0.01, # 1/fm^3
+general_parameters = {
+        "edens_min": 0.001, # GeV/fm^3
+        "edens_max": 0.5, # GeV/fm^3
+        "nB_min": 0.01, # 1/fm^3
+        "nB_mmax": 0.01, # 1/fm^3
+        "nQ_min": 0.01, # 1/fm^3
+        "nQ_mmax": 0.01, # 1/fm^3
+        "nS_min": 0.01, # 1/fm^3
+        "nS_mmax": 0.01, # 1/fm^3
 
-    "Dimension" = 4,
-    "Accuracy" = 1e-6,
-    "MAXITER" = 100,
-    "Parallized" = True,
-    "Number_of_cores" = 12,
+        "Dimension": 4,
+        "Accuracy": 1e-6,
+        "MAXITER": 100,
+        "Parallized": True,
+        "Number_of_cores": 12,
 
-    "EoS_table" = "HRG4D.dat"
+        "EoS_table": "HRG4D.dat"
 }
